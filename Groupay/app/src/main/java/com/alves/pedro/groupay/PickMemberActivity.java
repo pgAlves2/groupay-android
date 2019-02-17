1d0c682c1347aabed6a06dd1771cfea17a60e1a2
Pedro Guilherme Alves <contatopedroalves@gmail.com>
2019-02-17 10:31:03 -0300

    bug fix

---
+++ Groupay/app/src/main/java/com/alves/pedro/groupay/PickMemberActivity.java
@@ -0,0 +1,13 @@
+package com.alves.pedro.groupay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_member);
+    }
+}
